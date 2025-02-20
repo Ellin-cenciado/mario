779e64359b626c10294bfba1f5df692c2decd164
Ellin <ellinsb2k21@gmail.com>
2025-02-20 16:58:34 -0300

    modified:   src/App.tsx 	modified:   src/components/ContactPage.tsx 	modified:   src/components/GalleryPage.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import HomePage from "./components/HomePage";
+import GalleryPage from "./components/GalleryPage";
+import ContactPage from "./components/ContactPage";
+import { PageProvider } from "./context/PageContext";
 
 function App() {
  
   return (
     <>
       <HomePage />
+      <GalleryPage />
+      <ContactPage />
     </>
   )
   
